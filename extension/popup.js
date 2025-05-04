873f2382a14692a7516b849888ed88764665db0e
Mahamudul.Dev <mahamudul.dev@gmail.com>
2025-05-04 15:05:55 +0600

    Update popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -4,7 +4,7 @@ document.getElementById("sendCookies").addEventListener("click", () => {
       .map((c) => `${c.name}=${c.value}`)
       .join("; ");
 
-    await fetch("https://amazon-wishlist-sorter-api.codejet.dev/api/cookie/set", {
+    await fetch("http://localhost:5050/api/cookie/set", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ cookie: cookieHeader }),
